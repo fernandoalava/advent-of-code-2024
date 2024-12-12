b28803fa14df2f1c1c932e1c78afb9c109682e5f
Fernando Alava <falava@gmail.com>
2024-12-12 16:50:34 +0000

    finish day 11

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -1,4 +1,5 @@
 import scala.io.Source
+import scala.collection.mutable
 object Day11 {
   val testInput = "125 17"
 
@@ -9,37 +10,59 @@ object Day11 {
     input.split(" ").toVector
   }
 
-  def transformStore(stone: String): Vector[String] = {
-    stone match
-      case "0" => Vector("1")
-      case value if (value.length() % 2 == 0) => {
-        val middle = value.length() / 2
-        val firstValue = BigInt(value.substring(0, middle))
-        val secondValue = BigInt(value
-              .substring(middle)
+  type Key = (Long, Int)
+
+  // This is not good, it totally breaks the idea of using FP, I need to figure out a better way to do this.
+  var cache: mutable.Map[Key, Long] = mutable.Map.empty
+
+  def blink(
+      stone: Long,
+      blinks: Int
+  ): Long = {
+    if (blinks == 0) {
+      return 1L
+    }
+    val a = (stone, blinks)
+    if (cache.contains(a)) {
+      return cache.get(a).get
+    }
+    val nextValue = stone match {
+      case 0L =>
+        blink(1L, blinks - 1)
+      case value if (value.toString().length() % 2 == 0) => {
+        val v = value.toString()
+        val middle = v.length() / 2
+        val firstValue = v.substring(0, middle).toLong
+        val secondValue = v
+          .substring(middle)
+          .toLong
+        blink(
+          firstValue,
+          blinks - 1
         )
-        Vector(
-          s"${firstValue}",
-          s"${secondValue}"
-        )
+          + blink(
+            secondValue,
+            blinks - 1
+          )
       }
       case value => {
-        val result = BigInt(value) * BigInt(2024)
-        Vector(s"${result}")
+        blink(value.toLong * 2024L, blinks - 1)
       }
+    }
+    cache ++= Map((stone, blinks) -> nextValue)
+    return nextValue
   }
 
-  def puzzle1(input: Vector[String], blinks: Int): Int = {
-    val stones = blinks match
-      case 0 => input
-      case _ =>
-        (0 until blinks).foldLeft(input)((newInput, blink) => newInput.flatMap(transformStore))
-    stones.size
+  def puzzle1(input: Vector[String], blinks: Int): Long = {
+    input
+      .map(_.toLong)
+      .map(stone => blink(stone, blinks))
+      .sum
   }
 }
 
 @main def mainDay11() = {
-//   println(Day11.puzzle1(Day11.parseInput(Day11.testInput), 25))
-//   println(Day11.puzzle1(Day11.parseInput(Day11.getInput()), 25))
+  println(Day11.puzzle1(Day11.parseInput(Day11.testInput), 75))
+  println(Day11.puzzle1(Day11.parseInput(Day11.getInput()), 25))
   println(Day11.puzzle1(Day11.parseInput(Day11.getInput()), 75))
 }
